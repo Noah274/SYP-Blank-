a1ad8ea83a7e615c4b45b191593f5bb895abc4be
Noah274 <85834180+Noah274@users.noreply.github.com>
2022-12-01 11:22:28 +0100

    added the speed factor of the player movement

--- DungeonGame/Assets/Scripts/Playermovement.cs
+++ DungeonGame/Assets/Scripts/Playermovement.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class Playermovement : MonoBehaviour
-{ 
+{
+    public float speed;
     void Update()
     {
+        
         float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(inputX , inputY, 0);
+        Vector3 movement = new Vector3(inputX *speed, inputY *speed, 0);
 
 
         movement *= Time.deltaTime;
