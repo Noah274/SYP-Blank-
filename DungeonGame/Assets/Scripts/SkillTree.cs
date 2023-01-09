f9df7567874836b91cfc331305b315a504c46dda
Lorenz-Kasieczka <lorenz.kasieczka@gmail.com>
2023-01-09 08:55:09 +0100

    update

--- DungeonGame/Assets/Scripts/SkillTree.cs
+++ DungeonGame/Assets/Scripts/SkillTree.cs
@@ -7,21 +7,5 @@ using UnityEngine.UI;
 
 public class SkillTree : MonoBehaviour
 {
-    public Button fire_ball;
-
-    // Start is called before the first frame update
-    void start()
-    {
-        fire_ball = GetComponent<Button>(Fire_Ball);
-    }
-
-    public void Back()
-    {
-        SceneManager.LoadScene("StartScreen");
-    }
-    public void Fire()
-    {
-        Debug.Log("butten prest");
-        fire_ball.interactable = false;
-    }
+    
 }
