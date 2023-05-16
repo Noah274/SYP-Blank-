25f4dfdd80f106c67086614e24df2cabec5b87f3
Lorenz-Kasieczka <lorenz.kasieczka@gmail.com>
2023-05-16 12:34:52 +0200

    player shoots

--- DungeonGame/Assets/Scripts/Player/Playermovement.cs
+++ DungeonGame/Assets/Scripts/Player/Playermovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -6,17 +7,31 @@ using UnityEngine;
 public class Playermovement : MonoBehaviour
 {
     public float speed;
+    public Rigidbody2D rb;
+    public Weapon weapon;
+
+    Vector2 moveDirection;
+    Vector2 mousePosition;
     void Update()
     {
         
-        float inputX = Input.GetAxis("Horizontal");
-        float inputY = Input.GetAxis("Vertical");
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        
+        if(Input.GetMouseButtonDown(0))
+        {
+            weapon.Fire();
+        }
+        moveDirection = new Vector2(moveX, moveY).normalized;
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
 
-        Vector3 movement = new Vector3(inputX *speed, inputY *speed, 0);
-
-
-        movement *= Time.deltaTime;
-
-        transform.Translate(movement);
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveDirection.x * speed, moveDirection.y * speed);
+        
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = aimAngle;
     }
 }
