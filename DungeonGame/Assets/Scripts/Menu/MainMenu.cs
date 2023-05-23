17215ac4b239bdf0315ee9d3eeb96c22e3e3a60b
Lorenz-Kasieczka <lorenz.kasieczka@gmail.com>
2023-05-23 11:32:46 +0200

    deleted logs

--- DungeonGame/Assets/Scripts/Menu/MainMenu.cs
+++ DungeonGame/Assets/Scripts/Menu/MainMenu.cs
@@ -8,13 +8,11 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene("Scenes/MainGame-Test");
-        Debug.Log("Button Press");
     }
 
     public void GoBackToMenu()
     {
         SceneManager.LoadScene("StartScreen");
-        Debug.Log("Jonas Schönbaß hat deine Mom verändert");
     }
     
     public void SwitchToOptionMenu()
@@ -24,7 +22,6 @@ public class MainMenu : MonoBehaviour
         
     public void OnApplicationQuit()
     {
-        Debug.Log("My benis is hoat");
         Application.Quit();
     }
     
