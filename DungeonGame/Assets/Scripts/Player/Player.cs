1e09dfcbab84acfec0751fd628756320b1cd7c74
Paul Krenmayr <85543732+Paul-Krenmayr@users.noreply.github.com>
2023-05-23 15:58:03 +0200

    coin thingy not out of bounds

--- DungeonGame/Assets/Scripts/Player/Player.cs
+++ DungeonGame/Assets/Scripts/Player/Player.cs
@@ -3,55 +3,63 @@ using UnityEngine.SceneManagement;
 
 
 public class Player : MonoBehaviour
+{
+    enum SkillDef
     {
-        enum SkillDef
+        Non,
+        Fireball,
+        FireWall,
+        Meteor
+    };
+
+    public int maxHealth = 100;
+    public int currentHealth;
+    public HealthBar healthBar;
+    private SkillDef[] _skill;
+
+    public void SetPrimarySkill(int skillNum)
+    {
+        if (skillNum == 2)
         {
-            Non,
-            Fireball,
-            FireWall,
-            Meteor
-        };
-        public int maxHealth = 100;
-        public int currentHealth;
-        public HealthBar healthBar;
-        private SkillDef[] _skill;
-        
-       public void SetPrimarySkill(int skillNum)
-        {
-            if (skillNum == 2)
-            {
-                _skill[0] = SkillDef.Fireball;  
-            }
+            _skill[0] = SkillDef.Fireball;
         }
-        void Start()
+    }
+
+    void Start()
+    {
+        _skill = new SkillDef[3] { SkillDef.Non, SkillDef.Non, SkillDef.Non };
+        currentHealth = maxHealth;
+        healthBar.setMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            _skill = new SkillDef[3]{SkillDef.Non, SkillDef.Non, SkillDef.Non};
-            currentHealth = maxHealth;
-            healthBar.setMaxHealth(maxHealth);
+            TakeDamage(10);
         }
 
-        void Update()
+        if (Input.GetKeyDown(KeyCode.H))
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                TakeDamage(10);
-            }
-            if (Input.GetKeyDown(KeyCode.H))
-            {
-                TakeDamage(-10);
-            }
+            TakeDamage(-10);
         }
-    
+    }
+
     void TakeDamage(int damage)
     {
-        if (damage < 0 && (currentHealth-damage) >= maxHealth){
+        if (damage < 0 && (currentHealth - damage) >= maxHealth)
+        {
             currentHealth = maxHealth;
         }
-        else if (damage > 0 && (currentHealth - damage) <= 0){
+        else if (damage > 0 && (currentHealth - damage) <= 0)
+        {
             currentHealth = 0;
         }
-        else{
+        else
+        {
             currentHealth -= damage;
         }
+
         healthBar.setHealth(currentHealth);
     }
+}
