ed5ac6f3c9309b9827b51c55e1d2072f525e086f
JonasSchoenbass <Jonaskh2005@gmail.com>
2023-06-20 10:16:30 +0200

    update
    button

---
+++ DungeonGame/Assets/Scripts/BuyOptions.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyOptions : MonoBehaviour
+{
+    public void IncreasePlayerStats()
+    {
+        Debug.Log("I am stronger.... I am smarter.... I am better!!!! I AM BETTERRRR!!!!!!!");
+    }
+}
